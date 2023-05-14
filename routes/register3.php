63a8df970c3eaccaf1bd9d541707b41bda96f916
unknown <javier.irigoyen@ikasle.aeg.eus>
2023-05-14 13:54:52 +0200

    Pantalla Admin

---
+++ routes/register3.php
@@ -0,0 +1,49 @@
+<?php
+// Establecer la conexión con la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "e3p1";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+// Recuperar los valores de la solicitud
+$gmail = $_POST["gmail"];
+
+
+// Buscar el usuario en la tabla erabiltzailea
+$sql = "SELECT * FROM erabiltzailea WHERE gmail = '$gmail'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // Actualizar la contraseña del usuario
+
+  // $sql = " erabiltzailea SET name_e = '$name_e' , rola = '$rola' , jaiotze_data = '$jaiotze_data' WHERE gmail = '$gmail'";
+
+   $sql =  " DELETE FROM erabiltzailea WHERE gmail = '$gmail' ";
+
+  if ($conn->query($sql) === TRUE) {
+    // Devolver una respuesta exitosa
+    $response = array("success" => true);
+    echo json_encode($response);
+  } else {
+    // Devolver una respuesta con un error de actualización de la base de datos
+    $response = array("success" => false, "message" => "Error updating record: " . $conn->error);
+    echo json_encode($response);
+  }
+} else {
+  // Devolver una respuesta con un error de usuario no encontrado
+  $response = array("success" => false, "message" => "User not found");
+  echo json_encode($response);
+}
+
+// Cerrar la conexión con la base de datos
+$conn->close();
+?>
+
+
+
+
